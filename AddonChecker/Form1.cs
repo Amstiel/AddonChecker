db3ce074d7c1390d1c4363cb4446256ebb1724e0
Alexey Kunitsky <anonymouss.alex@gmail.com>
2016-07-24 13:07:22 +1000

    Added output
    Added search and output installed addons

--- AddonChecker/Form1.cs
+++ AddonChecker/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AddonChecker
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        static String DefaultPath = @"C:\Games\World of Warcraft\Interface\AddOns";
+        DirectoryInfo DirInfo = new DirectoryInfo(DefaultPath);
+        ArrayList ListItems = new ArrayList();
+        public MainForm()
         {
             InitializeComponent();
+            if (DirInfo.Exists)
+            {
+                string[] Dirs = Directory.GetDirectories(DefaultPath);
+                int YOffset = 10;
+                foreach (string s in Dirs)
+                {
+                    if (s.Contains("Blizzard")) continue;
+                    if (s.Contains("DBM-")) continue; //ingoring DBM addon
+                    Label lb = new Label();
+                    lb.Text = s;
+                    lb.AutoSize = true;
+                    lb.Left = 10;
+                    ListItems.Add(lb);
+                }
+
+                for (int i = 0; i < ListItems.Count; i++)
+                {
+                    ((Label)ListItems[i]).Top = 15 * i + YOffset;
+                    this.Controls.Add((Label)(ListItems[i]));
+                }
+            }
         }
     }
 }
